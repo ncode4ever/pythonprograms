4516ef11763e79a385fe072db9606c652be09f16
NH <nhegdes@gmail.com>
2025-05-04 16:51:52 +0530

    Added list1.py

---
+++ list1.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+print(thislist[2:])
+print(thislist[:5])
+print(thislist[-4:-1])
+print(thislist[-1:])
+print(thislist[:-1])
+
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
